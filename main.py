a5e2dfaa627a7af1c33b98b2b2d847ef7ee80485
Francis Tseng <f+accounts@frnsys.com>
2019-02-15 12:16:32 -0500

    init

---
+++ main.py
@@ -0,0 +1,29 @@
+import justext
+import requests
+import subprocess
+
+def site2epub(url, outfile):
+    resp = requests.get(url)
+    paragraphs = justext.justext(resp.content, justext.get_stoplist('English'))
+    html = []
+    for paragraph in paragraphs:
+        if not paragraph.is_boilerplate:
+            tag = paragraph.dom_path.rsplit('.', 1)[-1]
+            if paragraph.is_heading:
+                html.append('<{}>{}</{}>'.format(tag, paragraph.text, tag))
+            else:
+                html.append('<p>{}</p>'.format(paragraph.text))
+    with open('/tmp/site2epub.html', 'w') as f:
+        f.write('\n'.join(html))
+
+    subprocess.run(['pandoc', '-o', outfile, '/tmp/site2epub.html'])
+
+if __name__ == '__main__':
+    import sys
+    args = sys.argv[1:]
+    if len(args) < 2:
+        print('Specify the url and output filename')
+        sys.exit(1)
+
+    url, outfile = args
+    site2epub(url, outfile)
