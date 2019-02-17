e0db68117cf59fd820072da344bf345e09c47e58
Francis Tseng <f+accounts@frnsys.com>
2019-02-17 16:02:34 -0500

    incorporate more metadata

--- site2epub.py
+++ site2epub.py
@@ -1,28 +1,55 @@
 import os
 import justext
-import requests
 import subprocess
+from slugify import slugify
+from newspaper import Article
+from datetime import datetime
+
+tmpl = '''
+<html>
+<head>
+    <title>{title}</title>
+    <meta name="subtitle" content="{subtitle}">
+    <meta name="author" content="{author}">
+    <meta name="date" content="Created at {date}">
+    <meta name="subject" content="{keywords}">
+</head>
+<body>
+{body}
+</body>
+</html>
+'''
 
 def site2epub(url, outdir):
-    resp = requests.get(url)
-    paragraphs = justext.justext(resp.content, justext.get_stoplist('English'))
+    article = Article(url)
+    article.download()
+    article.parse()
+    article.nlp()
 
-    title = None
-    html = ['<p>Saved from: {}</p>'.format(url)]
+    paragraphs = justext.justext(article.html, justext.get_stoplist('English'))
+
+    html = []
     for paragraph in paragraphs:
         if not paragraph.is_boilerplate:
             tag = paragraph.dom_path.rsplit('.', 1)[-1]
             if paragraph.is_heading:
-                if title is None:
-                    title = paragraph.text
                 html.append('<{}>{}</{}>'.format(tag, paragraph.text, tag))
             else:
                 html.append('<p>{}</p>'.format(paragraph.text))
 
-    with open('/tmp/site2epub.html', 'w') as f:
-        f.write('\n'.join(html))
 
-    outfile = os.path.join(outdir, '{}.epub'.format(title))
+    title = article.title
+    with open('/tmp/site2epub.html', 'w') as f:
+        f.write(tmpl.format(
+            title=title,
+            author='; '.join(article.authors),
+            subtitle='Saved from: {}'.format(url),
+            keywords=', '.join(article.keywords),
+            date=datetime.now(),
+            body='\n'.join(html)))
+
+    fname = '{}.epub'.format(slugify(title))
+    outfile = os.path.join(outdir, fname)
     subprocess.run(['pandoc', '-o', outfile, '/tmp/site2epub.html'])
     return outfile
 
